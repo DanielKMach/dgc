785ff14876f87a3af437bf80820a08de93144e54
DanielKMach <danikoucher@gmail.com>
2024-10-10 16:26:51 -0300

    feat: Adiciona config ao BoxElement

--- src/canvas/BoxElement.zig
+++ src/canvas/BoxElement.zig
@@ -2,23 +2,38 @@ const std = @import("std");
 const Element = @import("element.zig");
 const Self = @This();
 
+pub const Config = struct {
+    char: ?u8 = null,
+    bgcolor: ?u8 = null,
+    fgcolor: ?u8 = null,
+};
+
 x: isize,
 y: isize,
 width: usize,
 height: usize,
+config: Config,
 
-pub fn init(x: isize, y: isize, width: usize, height: usize) Self {
-    return Self{ .x = x, .y = y, .width = width, .height = height };
+pub fn init(x: isize, y: isize, width: usize, height: usize, config: Config) Self {
+    return Self{
+        .x = x,
+        .y = y,
+        .width = width,
+        .height = height,
+        .config = config,
+    };
 }
 
 pub fn frag(self: Self, x: usize, y: usize, buf: []u8) void {
     if (x >= self.x and x < self.x + @as(isize, @intCast(self.width)) and y >= self.y and y < self.y + @as(isize, @intCast(self.height))) {
-        buf[0] = '#';
+        if (self.config.char) |c| buf[0] = c;
+        if (self.config.fgcolor) |c| buf[1] = c;
+        if (self.config.bgcolor) |c| buf[2] = c;
     }
 }
 
 test "boundary" {
-    const box = Self.init(0, 0, 16, 16);
+    const box = Self.init(0, 0, 16, 16, .{ .char = '#' });
     var buf: [3]u8 = std.mem.zeroes([3]u8);
 
     box.frag(15, 15, &buf);
@@ -35,7 +50,7 @@ test "boundary" {
 }
 
 test "element" {
-    const box = Self.init(0, 0, 16, 16);
+    const box = Self.init(0, 0, 16, 16, .{ .char = '#' });
     var element = try Element.new(&box, std.testing.allocator);
     defer element.deinit();
     var buf: [3]u8 = std.mem.zeroes([3]u8);
@@ -54,7 +69,7 @@ test "element" {
 }
 
 test "allocated element" {
-    const box = Self.init(0, 0, 16, 16);
+    const box = Self.init(0, 0, 16, 16, .{ .char = '#' });
     var element = try Element.new(box, std.testing.allocator);
     defer element.deinit();
     var buf: [3]u8 = std.mem.zeroes([3]u8);
@@ -73,7 +88,7 @@ test "allocated element" {
 }
 
 test "moving" {
-    var box = Self.init(0, 0, 16, 16);
+    var box = Self.init(0, 0, 16, 16, .{ .char = '#' });
     var buf: [3]u8 = std.mem.zeroes([3]u8);
 
     box.frag(0, 0, &buf);
@@ -92,7 +107,7 @@ test "moving" {
 }
 
 test "moving element" {
-    var box = Self.init(0, 0, 16, 16);
+    var box = Self.init(0, 0, 16, 16, .{ .char = '#' });
     var element = try Element.new(&box, std.testing.allocator);
     defer element.deinit();
     var buf: [3]u8 = std.mem.zeroes([3]u8);
--- src/games/Minesweeper.zig
+++ src/games/Minesweeper.zig
@@ -12,6 +12,8 @@ x: usize = 0,
 y: usize = 0,
 state: []u8,
 bombs: []u8,
+vline: *canvas.BoxElement,
+hline: *canvas.BoxElement,
 
 pub fn init(allocator: std.mem.Allocator, out: std.io.AnyWriter) !Self {
     var cvs = canvas.Canvas.init(out, 16, 6, allocator);
@@ -29,7 +31,14 @@ pub fn init(allocator: std.mem.Allocator, out: std.io.AnyWriter) !Self {
         }
     }
 
+    const vline = try allocator.create(canvas.BoxElement);
+    const hline = try allocator.create(canvas.BoxElement);
+    vline.* = canvas.BoxElement.init(0, 0, 1, 6, .{ .bgcolor = 238 });
+    hline.* = canvas.BoxElement.init(0, 0, 16, 1, .{ .bgcolor = 238 });
+
     try cvs.addElement(canvas.ImageElement.init(0, 0, 16, 6, state));
+    try cvs.addElement(vline);
+    try cvs.addElement(hline);
 
     return Self{
         .allocator = allocator,
@@ -38,19 +47,40 @@ pub fn init(allocator: std.mem.Allocator, out: std.io.AnyWriter) !Self {
         .height = 6,
         .state = state,
         .bombs = bombs,
+        .vline = vline,
+        .hline = hline,
     };
 }
 
 pub fn update(self: *Self, key: u8) !void {
-    switch (key) {
-        'j' => self.y += 1,
-        'k' => self.y -= 1,
-        'l' => self.x += 1,
-        'h' => self.x -= 1,
-        'P' => self.state[self.y * self.width + self.x] = 'P',
+    switch (std.ascii.toLower(key)) {
+        'j' => self.moveCursor(0, 1),
+        'k' => self.moveCursor(0, -1),
+        'l' => self.moveCursor(1, 0),
+        'h' => self.moveCursor(-1, 0),
+        'p' => {
+            const i = self.y * self.width + self.x;
+            switch (self.state[i]) {
+                'P' => self.state[i] = '#',
+                '#' => self.state[i] = 'P',
+                else => {},
+            }
+        },
         ' ' => self.open(self.x, self.y),
         else => {},
     }
+}
+
+pub fn moveCursor(self: *Self, dx: isize, dy: isize) void {
+    if (dx == 0 and dy == 0) return;
+    if (dx == 0 and self.y == 0 and dy == -1) return;
+    if (dx == 0 and self.y == self.height - 1 and dy == 1) return;
+    if (dy == 0 and self.x == 0 and dx == -1) return;
+    if (dy == 0 and self.x == self.width - 1 and dx == 1) return;
+    self.x = @as(usize, @intCast(@as(isize, @intCast(self.x)) + dx));
+    self.y = @as(usize, @intCast(@as(isize, @intCast(self.y)) + dy));
+    self.vline.x = @intCast(self.x);
+    self.hline.y = @intCast(self.y);
 }
 
 pub fn open(self: *Self, x: usize, y: usize) void {
@@ -75,6 +105,7 @@ pub fn open(self: *Self, x: usize, y: usize) void {
             if (dx == 0 and x == 0 or dy == 0 and y == 0) continue;
             const yy = y + dy - 1;
             const xx = x + dx - 1;
+            if (yy < 0 or yy >= self.height or xx < 0 or xx >= self.width) continue;
             if (self.bombs[yy * self.width + xx] == 'X') {
                 self.state[i] += 1;
             }
@@ -87,6 +118,7 @@ pub fn open(self: *Self, x: usize, y: usize) void {
                 if (dx == 0 and x == 0 or dy == 0 and y == 0) continue;
                 const yy = y + dy - 1;
                 const xx = x + dx - 1;
+                if (yy < 0 or yy >= self.height or xx < 0 or xx >= self.width) continue;
                 self.open(xx, yy);
             }
         }
